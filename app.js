bd3534d3fe5b0a5627d2f63529d7ef29d41754d5
aqsaanazz66 <81816238+aqsaanazz66@users.noreply.github.com>
2021-04-02 07:40:39 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+var list =document.getElementById("list") ;
+
+function addTodo(){
+ 
+   var todo_item = document.getElementById("todo_item")
+
+
+
+   
+
+var li = document.createElement("li")
+
+
+   
+var liText = document.createTextNode(todo_item.value)
+
+   
+li.appendChild(liText)
+
+
+
+
+var delBtn = document.createElement("button")
+
+var delText = document.createTextNode("DELETE")
+
+ delBtn.setAttribute("onclick","deleteItem(this)")
+ 
+delBtn.appendChild(delText)
+
+
+
+
+ var editBtn = document.createElement("button")
+ 
+var editText = document.createTextNode("EDIT")
+ 
+editBtn.setAttribute("onclick","editItem(this)")
+ 
+editBtn.appendChild(editText)
+
+
+
+ 
+li.appendChild(delBtn)
+ 
+li.appendChild(editBtn)
+ 
+list.appendChild(li)
+
+todo_item.value = " "
+
+console.log(li)
+
+}
